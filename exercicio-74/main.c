3d7206668f7b9e4a282f5d9f49629cdb7ffb5245
Thaiza Alves <87837910+Thaiza12@users.noreply.github.com>
2024-09-15 00:09:34 -0300

    Update main.c
    Add da atividade

---
+++ exercicio-74/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//Crie um programa que lê 6 valores inteiros e, em seguida, mostre na tela os valores lidos
+
+int main(){
+int num1, num2, num3, num4, num5, num6;
+
+printf("Informe 6 números inteiros: \n");
+scanf("%d %d %d %d %d %d", &num1,&num2,&num3,&num4,&num5,&num6);
+
+printf("Os números digitados são: %d %d %d %d %d %d", num1, num2, num3, num4, num5, num6);
+
+}
